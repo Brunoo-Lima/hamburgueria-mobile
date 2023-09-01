0e7dce9e20e7a62b3bcff59a287aa964ee36726d
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-09-01 14:08:04 -0300

    feat: add useContext for authentication

--- src/pages/Signin/signin.tsx
+++ src/pages/Signin/signin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import {
   View,
   Text,
@@ -8,7 +8,19 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import { AuthContext } from '../../contexts/AuthContext';
+
 export default function SignIn() {
+  const { user } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin() {
+    if (email === '' || password === '') {
+      return;
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.logo}>Hamburgueria</Text>
@@ -18,6 +30,8 @@ export default function SignIn() {
           placeholder="Email"
           placeholderTextColor="#fff"
           style={styles.input}
+          value={email}
+          onChangeText={setEmail}
         />
 
         <TextInput
@@ -25,9 +39,11 @@ export default function SignIn() {
           placeholderTextColor="#fff"
           style={styles.input}
           secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
         />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.buttonText}>Acessar</Text>
         </TouchableOpacity>
       </View>
@@ -59,6 +75,7 @@ const styles = StyleSheet.create({
     width: '95%',
     height: 40,
     backgroundColor: '#1D3056',
+    color: '#ffffff',
     marginBottom: 12,
     borderRadius: 4,
     paddingHorizontal: 8,
