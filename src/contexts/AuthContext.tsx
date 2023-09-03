99dbe13b4917c0fdcead29c5685a2cd972917dd2
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-09-03 10:38:22 -0300

    feat: staying logged in

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, ReactNode } from 'react';
+import { useState, createContext, ReactNode, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { api } from '../services/api';
@@ -7,6 +7,8 @@ type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
+  loadingAuth: boolean;
+  loading: boolean;
 };
 
 type UserProps = {
@@ -36,8 +38,31 @@ export function AuthProvider({ children }: AuthProviderProps) {
   });
 
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const isAuthenticated = !!user.name;
+
+  useEffect(() => {
+    async function getUser() {
+      const userInfo = await AsyncStorage.getItem('@hamburgueria');
+      let hasUser: UserProps = JSON.parse(userInfo || '{}');
+
+      if (Object.keys(hasUser).length > 0) {
+        api.defaults.headers.common[
+          'Authorization'
+        ] = `Bearer ${hasUser.token}`;
+
+        setUser({
+          id: hasUser.id,
+          name: hasUser.name,
+          email: hasUser.email,
+          token: hasUser.token,
+        });
+      }
+      setLoading(false);
+    }
+    getUser();
+  }, []);
 
   async function signIn({ email, password }: SignInProps) {
     setLoadingAuth(true);
@@ -71,7 +96,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider
+      value={{ user, isAuthenticated, signIn, loadingAuth, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
