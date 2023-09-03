b3da2178dc9865c4a9de4f45b71d794b96709d58
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-09-03 14:42:48 -0300

    feat: search category

--- src/pages/Order/order.tsx
+++ src/pages/Order/order.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -20,11 +20,31 @@ type RouteDetailParams = {
   };
 };
 
+type CategoryProps = {
+  id: string;
+  name: string;
+};
+
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order() {
   const route = useRoute<OrderRouteProps>();
   const navigation = useNavigation();
+
+  const [category, setCategory] = useState<CategoryProps[] | []>([]);
+  const [categorySelected, setCategorySelected] = useState<CategoryProps>();
+  const [amount, setAmount] = useState('1');
+
+  useEffect(() => {
+    async function loadInfo() {
+      const response = await api.get('/category');
+
+      setCategory(response.data);
+      setCategorySelected(response.data[0]);
+    }
+
+    loadInfo();
+  }, []);
 
   async function handleCloseOrder() {
     try {
@@ -47,9 +67,11 @@ export default function Order() {
         </TouchableOpacity>
       </View>
 
-      <TouchableOpacity style={styles.input}>
-        <Text style={{ color: '#fff' }}>Hamburguer</Text>
-      </TouchableOpacity>
+      {category.length !== 0 && (
+        <TouchableOpacity style={styles.input}>
+          <Text style={{ color: '#fff' }}>{categorySelected?.name}</Text>
+        </TouchableOpacity>
+      )}
 
       <TouchableOpacity style={styles.input}>
         <Text style={{ color: '#fff' }}>X Bacon</Text>
@@ -59,9 +81,10 @@ export default function Order() {
         <Text style={styles.qtdText}>Quantidade</Text>
         <TextInput
           style={[styles.input, { width: '60%', textAlign: 'center' }]}
-          placeholder="1"
           placeholderTextColor="#fff"
           keyboardType="numeric"
+          value={amount}
+          onChangeText={setAmount}
         />
       </View>
 
