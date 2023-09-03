91e1b0ccf91bdde857ba389b2e798363e3c928d7
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-09-03 16:27:28 -0300

    style: item list style

--- src/components/ListItem/items.tsx
+++ src/components/ListItem/items.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+import { Feather } from '@expo/vector-icons';
 
 interface ItemProps {
   data: {
@@ -13,14 +15,32 @@ interface ItemProps {
 export function ListItem({ data }: ItemProps) {
   return (
     <View style={styles.container}>
-      <Text style={styles.products}>{data.name}</Text>
+      <Text style={styles.items}>
+        {data.amount} - {data.name}
+      </Text>
+
+      <TouchableOpacity>
+        <Feather name="trash-2" size={25} color="#FF3F4b" />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  products: {
+  container: {
+    flex: 1,
+    backgroundColor: '#1D3056',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    marginBottom: 12,
+    borderRadius: 4,
+    borderWidth: 0.3,
+    borderColor: '#D7F9FF',
+  },
+  items: {
     color: '#fff',
   },
 });
